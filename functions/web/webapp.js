3002c450694c34ae8fa91cd0f515c304c5b87aab
Amit Shah <amitshah@Amits-MacBook-Pro.local>
2017-09-13 23:03:36 -0400

    add presale registration workflow and db access

--- functions/web/webapp.js
+++ functions/web/webapp.js
@@ -2,16 +2,23 @@
 //https://github.com/firebase/functions-samples/tree/master/fulltext-search
 
 const functions = require('firebase-functions');
-var firebase = require('firebase-admin');
+var admin = require('firebase-admin');
+const validator = require('validator');
 
+const privateConfigs = functions.config();
 const express = require('express');
 const engines = require('consolidate');
 const mcapi = require('mailchimp-api/mailchimp');
-const mailchimp_key = functions.config().mailchimp.key;
-const mailchimp_listid = functions.config().mailchimp.listid;
+const mailchimp_key = privateConfigs.mailchimp.key;
+const mailchimp_listid = privateConfigs.mailchimp.listid;
+const mailchimp_presale_listid = privateConfigs.mailchimp.presale_listid;
+
+admin.initializeApp(privateConfigs.firebase);
+
+const database = admin.database();
+
 
 try {
-    //TODO: dont commit this!
     var mc = new mcapi.Mailchimp(mailchimp_key);
 
 } catch (error) {
@@ -22,17 +29,17 @@ try {
 
 const app = express();
 
-
 app.engine('hbs', engines.handlebars);
-app.set('views', './web/views');
+app.set('views', './web/views/');
 app.set('view engine', 'hbs');
 
 app.get('/', (request, response) => {
+
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    //response.send(`${Date.now()}`);
+
     const username = request.params.username;
 
-    response.render('index', { username })
+    response.render('index2', { username })
 })
 
 
@@ -45,31 +52,54 @@ app.get('/slack', (request, response) => {
 //subscribe user to our mailchimp
 app.post('/subscribe', (request, response) => {
     console.log('body: ' + JSON.stringify(request.body));
-
-    mc.lists.subscribe({ id: mailchimp_listid, email: { email: request.body.email } }, function (data) {
+    if(validator.isEmail(request.body.email)){
+      mc.lists.subscribe({ id: mailchimp_listid, email: { email: request.body.email } }, function (data) {
         response.setHeader('Content-Type', 'application/json');
         response.send(JSON.stringify({ "message": "Please check your email and confirm your subscription!" }));
     }, function function_name(error) {
-        console.log(error)
-
+        console.log(error);
         response.statusCode = 401;
-        response.send(JSON.stringify({ "message": "error setting up subscription, please retry" }));
-    })
-    //mc.lists.subscribe({id: req.params.id, email:{email:req.body.email}}, function(data) {
-    //   req.session.success_flash = 'User subscribed successfully! Look for the confirmation email.';
-    //   res.redirect('/lists/'+req.params.id);
-    // },
-    // function(error) {
-    //   if (error.error) {
-    //     req.session.error_flash = error.code + ": " + error.error;
-    //   } else {
-    //     req.session.error_flash = 'There was an error subscribing that user';
-    //   }
-    //   res.redirect('/lists/'+req.params.id);
-    // });
-    // response.setHeader('Content-Type', 'application/json');
-    // response.send(JSON.stringify({ a: 1 }));
+        if(error.code == 214){
+            response.send(JSON.stringify({ "message": "Already Registered"}));
+        }else{
+            response.send(JSON.stringify({ "message": "retry"}));
+        }
+
+    });
+
+    }else{
+         response.statusCode = 401;
+         response.send(JSON.stringify({ "message":"invalid email"}));
+    }
 });
+
+
+//subscribe user to our mailchimp presale
+app.post('/presale/subscribe', (request, response) => {
+    console.log(request.body)
+    console.log(validator.isEmail(request.body.email));
+    if(validator.isEmail(request.body.email)){
+        mc.lists.subscribe({ id: mailchimp_presale_listid, email: { email: request.body.email } }, function (data) {
+            database.ref('presale/users').push(request.body);
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({ "message": "Please check your email and confirm your subscription!" }));
+        }, function function_name(error) {
+            console.log(error)
+            console.log(error);
+            response.statusCode = 401;
+            if(error.code == 214){
+                response.send(JSON.stringify({ "message": "Already Registered"}));
+            }else{
+                response.send(JSON.stringify({ "message": "retry"}));
+            }
+        });
+
+    }else{
+         response.statusCode = 401;
+         response.send(JSON.stringify({ "message":"invalid email"}));
+    }
+});
+
 
 exports.app = functions.https.onRequest(app);
 
