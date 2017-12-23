0d5d3d55d7d587fd0e2da278149d34219b654ff1
Enda Xue <xueenda@gmail.com>
2017-12-23 00:09:43 -0500

    Add web push notifications

---
+++ public/assets/js/webnotification.js
@@ -0,0 +1,26 @@
+var myNotification = new Notify('Yo dawg!', {
+  body: 'This is an awesome notification',
+  notifyShow: onNotifyShow
+});
+
+function onNotifyShow() {
+  console.log('notification was shown!');
+}
+
+
+myNotification.show();
+
+if (!Notify.needsPermission) {
+  // myNotification();
+} else if (Notify.isSupported()) {
+  Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+}
+
+function onPermissionGranted() {
+  console.log('Permission has been granted by the user');
+  // myNotification();
+}
+
+function onPermissionDenied() {
+  console.warn('Permission has been denied by the user');
+}
